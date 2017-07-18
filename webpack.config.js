d0420267f52ed1350c602d29582890b87240d889
jackyyang <jackey088@hotmail.com>
2017-07-18 15:13:06 +0800

    glob 读取v页面路径

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+const glob = require('glob');
+
+const pageFiles = glob.sync('src/projects/**/v/**/*', {
+	nodir: true
+});
+
+console.log(pageFiles);
+
 module.exports = {
 	entry: './src/assets/scripts/common/common',
 	output: {
-		path: __dirname +'/dist',
+		path: path.resolve(__dirname,'./dist'),
+		publicPath: '/dist',
 		filename: 'build.js'
 	}
 };
