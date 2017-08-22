61f9475827065f8ceeef6776cec3a58b2b2655ad
jackeyyang <jackeyyang@coderedfour.com>
2017-08-22 23:15:25 +0800

    asdf

--- notebook/mark.md
+++ notebook/mark.md
@@ -7,3 +7,7 @@ git add .
 git commit -m 'update .gitignore'
 git reset --hard e377f60e28c8b84158 // 回退到指定版本
 git reset --hard HEAD^ // 回退
+git checkout . // 本地所有修改的。没有的提交的，都返回到原来的状态
+git stash // 把所有没有提交的修改暂存到stash里面。可用git stash pop回复。
+git reset --hard HASH // 返回到某个节点，不保留修改。
+git reset --soft HASH // 返回到某个节点。保留修改
--- webpack/myConfig.js
+++ webpack/myConfig.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-exports.JSP_DEV_PATH = '/Users/jackey/html5/';
-exports.ECLIPSE = false;
-=======
-exports.JSP_DEV_PATH = 'E:\\apache-tomcat-9.0.0.M13-windows-x64\\apache-tomcat-9.0.0.M13\\webapps';
+exports.JSP_DEV_PATH = '/usr/local/apache-tomcat-8.0.45/webapps';
 exports.ECLIPSE = true;
->>>>>>> 7f76cc5575a7ca407bba8917cdbd829e7529443c
