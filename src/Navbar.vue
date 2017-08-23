df6a513289e6b9b06131cbbda587ea62f70dd5fc
jackeyyang <jackeyyang@coderedfour.com>
2017-08-24 01:46:03 +0800

    node-sass sass-loader

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,5 +1,9 @@
 <template>
-<header>navbar</header>
+<header class="header-wrap">
+	<a href="#" class="logo">
+		<span class="logo-lg">Choice</span>		
+	</a>
+</header>
 </template>
 
 <script>
@@ -7,3 +11,21 @@ export default {
 	name: 'va-navbar'
 }
 </script>
+
+<style lang="scss">
+	.header-wrap{
+		position: relative;
+		max-height: 100px;
+		z-index: 1000;
+	
+		.logo{
+			display: block;
+			float: left;
+			line-height: 50px;
+			font-size: 20px;
+			font-weight: 300;
+			text-align: center;
+			width: 230px;
+		}
+	}
+</style>
