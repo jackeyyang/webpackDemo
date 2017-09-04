6a0ab9470bee114724bd84c78f59f82bb5311fe2
jackyyang <jackey088@hotmail.com>
2017-09-04 11:52:06 +0800

    content wrapper breadCrumbs

--- src/ContentWrap.vue
+++ src/ContentWrap.vue
@@ -1,5 +1,13 @@
 <template>
-	<div class="content"></div>
+	<div class="content">
+		<div class="content-header">
+            <div class="leftside-content-header">
+                <ul class="breadcrumbs">
+                    <li><i class="fa fa-home" aria-hidden="true"></i><a href="#">Dashboard</a></li>
+                </ul>
+            </div>
+        </div>
+	</div>
 </template>
 
 
