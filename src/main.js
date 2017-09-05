d2f9eea728fd077ea0e84b03983ba3a4780d740c
jackyyang <jackey088@hotmail.com>
2017-09-05 14:41:18 +0800

    http-server on dist run

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import '@/lib/css'
-import '@/lib/bootstrap/bootstrap.min'
+// import '@/lib/bootstrap/bootstrap.min'
+import 'bootstrap'
 import '@/lib/nano-scroller/nano-scroller'
 import '@/lib/template-script.min'
 import '@/lib/template-init.min'
